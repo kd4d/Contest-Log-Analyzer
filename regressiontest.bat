d4c1809236d26f620f76d8d010cb9d7b5e051198
Mark Bailey <kd4d@comcast.net>
2025-08-11 23:47:37 +0000

    Add another compare for WPX CW and updated regresstiontest.bat.

--- regressiontest.bat
+++ regressiontest.bat
@@ -2,4 +2,5 @@ python main_cli.py --report all 2024\cq-ww-cw\k3lr.log 2024\cq-ww-CW\w3lpl.log 2
 python main_cli.py --report all 2024\ARRL-SS-CW\kd4d.log 2024\ARRL-SS-CW\k3mm.log 2024\ARRL-SS-CW\aa3b.log
 python main_cli.py --report all 2025\CQ-160-cw\kd4d.log 2025\cq-160-cw\n0ni.log
 python main_cli.py --report all 2025\cq-wpx-cw\k3lr.log 2025\cq-wpx-cw\kc1xx.log
+python main_cli.py --report all 2025\cq-wpx-cw\ni4w.log 2025\cq-wpx-cw\kb4dx.log
 python main_cli.py --report all 2025\NAQP-CW\aug\k3aj.log 2025\NAQP-CW\aug\wn4afp.log
