5d8fe6ecadcd8f46863e079be2d282148ccb6fc0
Mark Bailey <kd4d@comcast.net>
2025-08-14 01:25:13 +0000

    More changes to try to fix scoring for NAQP.

--- regressiontest.bat
+++ regressiontest.bat
@@ -5,3 +5,4 @@ python main_cli.py --report all 2025\cq-wpx-cw\k3lr.log 2025\cq-wpx-cw\kc1xx.log
 python main_cli.py --report all 2025\cq-wpx-cw\ni4w.log 2025\cq-wpx-cw\kb4dx.log
 python main_cli.py --report all 2025\NAQP-CW\aug\k3aj.log 2025\NAQP-CW\aug\wn4afp.log
 python main_cli.py --report all 2024\ARRL-10\ve3ej.log 2024\arrl-10\vp2vmm.log
+python main_cli.py --report all 2025\NAQP-CW\aug\k3aj.log 2025\NAQP-CW\aug\wx3b.log
