4f25438b249b41e7802efb7968fb9c007af7a466
Mark Bailey <kd4d@comcast.net>
2025-08-24 16:27:45 +0000

    Updated multiplier reports have STPROV ID and name now for CQ-160 contest.

--- contest_tools/contest_specific_annotations/cq_160_multiplier_resolver.py
+++ contest_tools/contest_specific_annotations/cq_160_multiplier_resolver.py
@@ -6,7 +6,7 @@
 # Author: Mark Bailey, KD4D
 # Contact: kd4d@kd4d.org
 # Date: 2025-08-24
-# Version: 0.30.41-Beta
+# Version: 0.40.0-Beta
 #
 # Copyright (c) 2025 Mark Bailey, KD4D
 #
@@ -14,8 +14,13 @@
 #          (https://www.mozilla.org/MPL/2.0/)
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0.
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # --- Revision History ---
+## [0.40.0-Beta] - 2025-08-24
+### Changed
+# - Modified resolver to create and populate a STPROV_MultName column
+#   to provide full names for State/Province multipliers.
 ## [0.30.41-Beta] - 2025-08-24
 ### Fixed
 # - Corrected logic to populate the DXCC multiplier value column with the
@@ -31,7 +36,7 @@
 import pandas as pd
 import os
 from ..core_annotations._core_utils import AliasLookup
-from typing import Optional
+from typing import Optional, Tuple
 
 def resolve_multipliers(df: pd.DataFrame, my_location_type: Optional[str]) -> pd.DataFrame:
     """
@@ -41,21 +46,22 @@ def resolve_multipliers(df: pd.DataFrame, my_location_type: Optional[str]) -> pd
         return df
 
     df['STPROV_Mult'] = pd.NA
+    df['STPROV_MultName'] = pd.NA
     df['DXCC_Mult'] = pd.NA
     df['DXCC_MultName'] = pd.NA
 
-    root_dir = os.environ.get('CONTEST_LOGS_REPORTS', '').strip().strip('"').strip("'")
+    root_dir = os.environ.get('CONTEST_LOGS_REPORTS', '').strip().strip("'").strip('"')
     data_dir = os.path.join(root_dir, 'data')
     alias_lookup = AliasLookup(data_dir, 'NAQPmults.dat')
 
-    def get_stprov_mult(row):
+    def get_stprov_mult(row) -> Tuple[Optional[str], Optional[str]]:
         if row.get('DXCCName') in ["United States", "Canada"]:
             location = row.get('RcvdLocation', '')
-            mult_abbr, _ = alias_lookup.get_multiplier(location)
-            return mult_abbr
-        return pd.NA
+            mult_abbr, mult_name = alias_lookup.get_multiplier(location)
+            return mult_abbr, mult_name
+        return pd.NA, pd.NA
 
-    df['STPROV_Mult'] = df.apply(get_stprov_mult, axis=1)
+    df[['STPROV_Mult', 'STPROV_MultName']] = df.apply(get_stprov_mult, axis=1, result_type='expand')
 
     df['DXCC_Mult'] = df['DXCCPfx']
     df['DXCC_MultName'] = df['DXCCName']
