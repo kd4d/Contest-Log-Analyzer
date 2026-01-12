b9630f8d3d4795a471e6811a1890dffc7ae23be7
Mark Bailey <markkd4d@gmail.com>
2026-01-12 22:51:11 +0000

    chore: remove version cruft from headers (batch 6/12) - Remove Author/Date/Version lines from file headers - Remove revision history sections - Cleaned files: contest_specific_annotations (_arrl_dx_utils.py, arrl_10_parser.py, arrl_dx_parser.py, arrl_fd_parser.py, cq_160_parser.py, cq_wpx_prefix.py, iaru_hf_parser.py, naqp_parser.py, wae_parser.py)

--- contest_tools/contest_specific_annotations/arrl_dx_parser.py
+++ contest_tools/contest_specific_annotations/arrl_dx_parser.py
@@ -3,11 +3,6 @@
 # Purpose: This module provides a custom parser for the ARRL DX Contest, which
 #          has a highly asymmetric exchange format.
 #
-#
-# Author: Gemini AI
-# Date: 2025-10-01
-# Version: 0.90.0-Beta
-#
 # Copyright (c) 2025 Mark Bailey, KD4D
 # Contact: kd4d@kd4d.org
 #
@@ -17,9 +12,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# --- Revision History ---
-# [0.90.0-Beta] - 2025-10-01
-# Set new baseline version for release.
 
 import pandas as pd
 import re
